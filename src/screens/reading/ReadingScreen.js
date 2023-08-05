561f276b8a3c78ab4addbd799deda37144d789e8
Luis Espana <fdh.luis.angel@gmail.com>
2023-08-04 23:19:40 -0500

    :construction: Work in progress :: Showing description, (reading view)

--- src/screens/reading/ReadingScreen.js
+++ src/screens/reading/ReadingScreen.js
@@ -1,34 +1,75 @@
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, ScrollView, SafeAreaView, Image, View } from 'react-native';
 import React from 'react';
-import { useEffect } from 'react';
 import useQuestions from '../../hooks/useQuestions';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import NavBar from '../../components/NavBar';
 import LoadingOverlay from '../../components/LoadingOverlay';
+import SelectDropdown from 'react-native-select-dropdown'
 
 
 export default function ReadingScreen(props) {
     const [isLoading, setIsLoading] = useState(false);
     const { getAll } = useQuestions();
     const user = useSelector((state) => state.user);
-    const {item} = props.route.params;
+    const { item } = props.route.params;
 
-    console.log(item);
+    //console.log(item);
+
+    const getOptions = (word) => {
+        let options = [];
+        if (item?.questions) {
+            item?.questions?.forEach((q) => {
+                if (q.title === word.replace('@', '')) {
+                    options = q.options;
+                }
+            });
+        }
+        return options;
+    }
 
     return (
         <SafeAreaView style={styles.container}>
-            <NavBar navigation={props.navigation} title={'Reading view in progress'}/>
+            <NavBar navigation={props.navigation} title={'Reading view in progress'} />
             <ScrollView style={styles.scrollView}>
-                <Text style={styles.itemText}>{JSON.stringify(item)}</Text>  
-                {                    
-                    /*
-                    <Text style={styles.itemText}>{item.type}</Text>         
-                    */
+                <Text>{item.title}</Text>
+
+
+                <Image source={{ uri: item.image }} />
+                
+            
+                <View style={styles.description}>
+                {
+                    item?.description && item?.description?.split(' ').map((word, i) => {
+                        if (word.includes('@')) {
+                            return (
+                                <SelectDropdown
+                                    key={i}
+                                    data={getOptions(word).map( (opt, j) =>opt?.description )}
+                                    onSelect={(selectedItem, index) => {
+                                        console.log(selectedItem, index)
+                                    }}
+                                    buttonTextAfterSelection={(selectedItem, index) => {
+                                        // text represented after item is selected
+                                        // if data array is an array of objects then return selectedItem.property to render after item is selected
+                                        return selectedItem
+                                    }}
+                                    rowTextForSelection={(item, index) => {
+                                        // text represented for each item in dropdown
+                                        // if data array is an array of objects then return item.property to represent item in dropdown
+                                        return item
+                                    }}
+                                />
+                            );
+                        } else {
+                            return <Text key={i} style={styles.word}>{word}</Text>
+                        }
+                    })
                 }
-                  
+                </View>               
+
             </ScrollView>
-            <LoadingOverlay isLoading={isLoading}/>
+            <LoadingOverlay isLoading={isLoading} />
         </SafeAreaView>
     )
 }
@@ -44,4 +85,11 @@ const styles = StyleSheet.create({
     scrollView: {
         marginTop: 50
     },
+    description : {
+        flexDirection:'row',
+        flexWrap: 'wrap'
+    },
+    word : {
+        marginEnd: 4
+    }
 })
